5db96b453a90ec9aef63394bd67e17e1224a6346
srihari-m-s <srihari.magadi@gmail.com>
2023-12-09 17:47:19 +0530

    feat: Add static map image

--- client/src/pages/Cabs/SearchPage/SearchPage.jsx
+++ client/src/pages/Cabs/SearchPage/SearchPage.jsx
@@ -2,16 +2,28 @@ import "./SearchPage.scss";
 import { Flex, Divider } from "antd";
 import { IoArrowBack } from "react-icons/io5";
 import BookingLocation from "../../../components/Cabs/BookingLocation/BookingLocation";
+import placeholder from "../../../assets/cabs/placeholder";
 
 export default function SearchPage() {
   return (
     <div className="search-page-wrapper">
-      <button type="button" className="btn-base">
+      <button
+        type="button"
+        className="btn-base"
+        style={{ marginBottom: "3rem" }}
+      >
         <IoArrowBack size={24} /> Pick-up
       </button>
 
-      <div className="booking-location-container" style={{ padding: "0 1rem" }}>
-        <BookingLocation />
+      <div className="booking-location-container">
+        <div className="" style={{ padding: "0 1rem" }}>
+          <BookingLocation />
+        </div>
+        <img src={placeholder.map} alt="Map" className="map-image" />
+      </div>
+
+      <div className="favorites-container">
+        <h3>Favorites</h3>
       </div>
     </div>
   );
