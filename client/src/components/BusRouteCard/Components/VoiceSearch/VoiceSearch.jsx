0f07277136efbba25a6fd3e7613b7e6b02572d34
srihari-m-s <srihari.magadi@gmail.com>
2023-12-21 17:46:58 +0530

    feat: Enforce timeout using JS and update UI for better feedback

--- client/src/components/BusRouteCard/Components/VoiceSearch/VoiceSearch.jsx
+++ client/src/components/BusRouteCard/Components/VoiceSearch/VoiceSearch.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useRef, useState } from "react";
-import MicImage from "../../../../assets/busbooking/micImg.svg";
+import { useEffect, useState } from "react";
+// import MicImage from "../../../../assets/busbooking/micImg.svg";
 import { SpeechRecognition } from "@capacitor-community/speech-recognition";
 import { Button, Drawer, Spin } from "antd";
 import "./VoiceSearch.scss";
+import { LuMic } from "react-icons/lu";
 
 export default function VoiceSearch({
   setLocationQuery,
@@ -14,6 +15,7 @@ export default function VoiceSearch({
   const [open, setOpen] = useState(false);
   const [searchValue, setSearchValue] = useState("");
   const [placeholder, setPlaceholder] = useState("Try saying something....");
+  const [isTimedout, setIsTimedout] = useState(false);
 
   useEffect(() => {
     async function getPermission() {
@@ -26,16 +28,28 @@ export default function VoiceSearch({
     getPermission();
   }, []);
 
+  // Timeout useEffect
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (!searchValue && open) {
+        handleTimeout();
+      }
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, [searchValue, handleTimeout, open]);
+
   async function startRecording() {
     const checkPermissions = await SpeechRecognition.checkPermissions();
-    if (checkPermissions.speechRecognition !== 'granted') {
-      await SpeechRecognition.requestPermissions()
+    if (checkPermissions.speechRecognition !== "granted") {
+      await SpeechRecognition.requestPermissions();
     } else {
       setOpen(true);
     }
     const available = await SpeechRecognition.available();
     if (available) {
       // setRecording(true);
+      setIsTimedout(false);
       SpeechRecognition.start({
         language: "en-US",
         // maxResults: 2,
@@ -59,6 +73,8 @@ export default function VoiceSearch({
     setOpen(false);
     setPlaceholder("Try saying something...");
     setSearchValue("");
+    setIsTimedout(false);
+    // setRecording(false);
 
     if (cancelled) {
       return;
@@ -84,37 +100,68 @@ export default function VoiceSearch({
     setPlaceholder("Try again...");
   }
 
-  // ClearStates
+  // Handle close
   function onClose() {
-    setOpen(false);
-    setSearchValue("");
     stopRecording(true);
+  }
+
+  // handle Timeout
+  function handleTimeout() {
+    SpeechRecognition.stop();
+    SpeechRecognition.removeAllListeners();
+    setIsTimedout(true);
+    setSearchValue("");
+    setPlaceholder("Didn't catch that. Try again.");
   }
 
   return (
     <div className="voice-search-wrapper">
       <button type="button" className="modal-trigger" onClick={handleMicClick}>
-        <img src={MicImage} width="30" height="30" />
+        {/* <img src={MicImage} width="30" height="30" /> */}
+        <LuMic size={30} color="white" />
       </button>
       <Drawer
         title={`Search ${title === "From" ? "Departure" : "Destination"}`}
         placement={"bottom"}
-        closable={true}
+        closable={false}
         onClose={onClose}
         open={open}
         key={"bottom"}
-        height={"200px"}
+        height={"250px"}
         classNames={{ body: "drawer-body" }}
       >
         <div className="drawer-content">
-          <div className="modal-mic">
-            <img src={MicImage} width="30" height="30" />
+          <div
+            className="modal-mic"
+            style={{
+              boxShadow: `${
+                searchValue
+                  ? ""
+                  : isTimedout
+                  ? "0px 0px 5px 5px #33333355"
+                  : "0px 0px 5px 5px #fd5901"
+              }`,
+            }}
+          >
+            {/* <img src={MicImage} width="30" height="30" /> */}
+            <LuMic
+              size={30}
+              color={`${
+                searchValue ? "#fd5901" : isTimedout ? "gray" : "white"
+              }`}
+            />
           </div>
           {!searchValue ? (
-            <>
-              <p>{placeholder}</p>
-              <Spin size="small" />
-            </>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                gap: "0.5rem",
+              }}
+            >
+              <p style={{ fontSize: "1.25rem" }}>{placeholder}</p>
+              {!isTimedout && <Spin size="small" />}
+            </div>
           ) : (
             <p className={` ${searchValue ? "search-result" : ""}`}>
               Did you mean{" "}
@@ -126,23 +173,41 @@ export default function VoiceSearch({
           )}
         </div>
         <div className="action-buttons">
-          <Button
-            type="primary"
-            onClick={() => stopRecording()}
-            size="large"
-            htmlType="button"
-          >
-            Yes
-          </Button>
-          <Button
-            type="primary"
-            danger
-            onClick={() => handleNoClick()}
-            size="large"
-            htmlType="button"
-          >
-            No
-          </Button>
+          {!isTimedout ? (
+            searchValue && (
+              <>
+                <Button
+                  type="primary"
+                  onClick={() => stopRecording()}
+                  size="middle"
+                  htmlType="button"
+                  style={{ paddingInline: "2rem" }}
+                >
+                  Yes
+                </Button>
+                <Button
+                  type="primary"
+                  danger
+                  onClick={() => handleNoClick()}
+                  size="middle"
+                  htmlType="button"
+                  style={{ paddingInline: "2rem" }}
+                >
+                  No
+                </Button>
+              </>
+            )
+          ) : (
+            <Button
+              type="primary"
+              onClick={() => handleNoClick()}
+              size="middle"
+              htmlType="button"
+              style={{ paddingInline: "2rem" }}
+            >
+              Retry
+            </Button>
+          )}
         </div>
       </Drawer>
     </div>
