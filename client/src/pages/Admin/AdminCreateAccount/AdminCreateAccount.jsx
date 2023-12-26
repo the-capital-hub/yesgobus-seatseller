9a825b01524f1e29ef164e1c5a913bdb316ce7ef
srihari-m-s <srihari.magadi@gmail.com>
2023-12-26 12:31:40 +0530

    feat: separate form from create account page

--- client/src/pages/Admin/AdminCreateAccount/AdminCreateAccount.jsx
+++ client/src/pages/Admin/AdminCreateAccount/AdminCreateAccount.jsx
@@ -1,8 +1,8 @@
-import { Button, Divider, Row, Col, Image } from "antd";
+import { Image } from "antd";
 import "./AdminCreateAccount.scss";
 // import { google, facebook } from "../../../assets/login";
-import { Link } from "react-router-dom";
 import { WatermarkIcon } from "../../../assets/contact";
+import CreateAccountForm from "./Components/CreateAccountForm/CreateAccountForm";
 
 export default function AdminCreateAccount() {
   return (
@@ -39,53 +39,7 @@ export default function AdminCreateAccount() {
             <Divider style={{ borderColor: "#000" }}>Or</Divider>
           </div> */}
 
-          <form className="create-account-form">
-            <input
-              type="text"
-              name="fullName"
-              id="admin-fullname"
-              className="create-input"
-              placeholder="Full Name"
-            />
-            <input
-              type="text"
-              name="email_phone"
-              id="admin-email_phone"
-              className="create-input"
-              placeholder="Email / Phone Number"
-            />
-            <input
-              type="password"
-              name="password"
-              id="admin-password"
-              className="create-input"
-              placeholder="Password"
-            />
-            <input
-              type="password"
-              name="confirmPassword"
-              id="admin-confirmPassword"
-              className="create-input"
-              placeholder="Confirm Password"
-            />
-
-            <div className="flex flex-col items-center gap-7 my-7">
-              <Link
-                to={"/admin/login"}
-                className="text-primary text-xl no-underline "
-              >
-                Login to Account
-              </Link>
-              <Button
-                htmlType="button"
-                type="primary"
-                shape="round"
-                size="large"
-              >
-                Create Account
-              </Button>
-            </div>
-          </form>
+          <CreateAccountForm />
         </div>
       </div>
     </div>
---
+++ client/src/pages/Admin/AdminCreateAccount/Components/CreateAccountForm/CreateAccountForm.jsx
@@ -0,0 +1,82 @@
+import { Button } from "antd";
+import { useState } from "react";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
+import { Link } from "react-router-dom";
+
+export default function CreateAccountForm() {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+
+  async function handleCreateAccountSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <form className="create-account-form" onSubmit={handleCreateAccountSubmit}>
+      <input
+        type="text"
+        name="fullName"
+        id="admin-fullname"
+        className="create-input"
+        placeholder="Full Name"
+      />
+      <input
+        type="text"
+        name="email_phone"
+        id="admin-email_phone"
+        className="create-input"
+        placeholder="Email / Phone Number"
+      />
+      <div className="create-input flex items-center gap-3">
+        <input
+          type={passwordVisible ? "text" : "password"}
+          name="password"
+          id="admin-password"
+          className="flex-grow h-full"
+          placeholder="Password"
+        />
+        <span
+          className="cursor-pointer"
+          onClick={() => setPasswordVisible(!passwordVisible)}
+        >
+          {passwordVisible ? (
+            <IoMdEye size={20} color="#8b8b8b" />
+          ) : (
+            <IoMdEyeOff size={20} color="#8b8b8b" />
+          )}
+        </span>
+      </div>
+      <div className="create-input flex items-center gap-3">
+        <input
+          type={confirmPasswordVisible ? "text" : "password"}
+          name="confirmPassword"
+          id="admin-confirmPassword"
+          className="flex-grow h-full"
+          placeholder="Confirm Password"
+        />
+        <span
+          className="cursor-pointer"
+          onClick={() => setConfirmPasswordVisible(!confirmPasswordVisible)}
+        >
+          {confirmPasswordVisible ? (
+            <IoMdEye size={20} color="#8b8b8b" />
+          ) : (
+            <IoMdEyeOff size={20} color="#8b8b8b" />
+          )}
+        </span>
+      </div>
+
+      <div className="flex flex-col items-center gap-7 my-7">
+        <Link
+          to={"/admin/login"}
+          className="text-primary text-xl no-underline "
+        >
+          Login to Account
+        </Link>
+        <Button htmlType="button" type="primary" shape="round" size="large">
+          Create Account
+        </Button>
+      </div>
+    </form>
+  );
+}
