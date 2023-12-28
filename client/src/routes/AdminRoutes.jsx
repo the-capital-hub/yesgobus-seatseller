4084d732928b308091402865ebd33af5da9abba0
MuhamedShahabasC <muhamedshahabasc@gmail.com>
2023-12-28 14:48:44 +0530

    Wallet page completed

--- client/src/routes/AdminRoutes.jsx
+++ client/src/routes/AdminRoutes.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import { AdminDashboard, AdminLayout } from "../pages/Admin";
+import { AdminDashboard, AdminLayout, AdminWallet } from "../pages/Admin";
 
 export default function AdminRoutes() {
   return (
     <Routes>
       <Route path="" element={<AdminLayout />}>
         <Route path="dashboard" element={<AdminDashboard />} />
-        <Route path="wallet" element={<p>Wallet</p>} />
+        <Route path="wallet" element={<AdminWallet />} />
         <Route path="records" element={<p>Wallet</p>} />
         <Route path="track-agents" element={<p>Track Agent</p>} />
         <Route path="track-buses" element={<p>Track Agent</p>} />
