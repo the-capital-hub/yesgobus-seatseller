b3b4ed09e35f976385001bd2da0d8fbf592d757a
Jijin vj <jijinvjvazhippilly@gmail.com>
2023-12-28 14:51:59 +0530

    track-agents and track-buses page created

--- client/src/routes/AdminRoutes.jsx
+++ client/src/routes/AdminRoutes.jsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { AdminDashboard, AdminLayout } from "../pages/Admin";
+import AdminTrackAgent from "../pages/Admin/AdminTrackAgent/AdminTrackAgent";
+import AdminTrackBuses from "../pages/Admin/AdminTrackBuses/AdminTrackBuses";
 
 export default function AdminRoutes() {
   return (
@@ -8,8 +10,8 @@ export default function AdminRoutes() {
         <Route path="dashboard" element={<AdminDashboard />} />
         <Route path="wallet" element={<p>Wallet</p>} />
         <Route path="records" element={<p>Wallet</p>} />
-        <Route path="track-agents" element={<p>Track Agent</p>} />
-        <Route path="track-buses" element={<p>Track Agent</p>} />
+        <Route path="track-agents" element={<AdminTrackAgent />} />
+        <Route path="track-buses" element={<AdminTrackBuses />} />
       </Route>
     </Routes>
   );
