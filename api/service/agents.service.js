509b0d001c80eb6bb91a5f1977df28ce3555ce89
Adithya <adithyahebbar32@gmail.com>
2023-12-29 16:40:26 +0530

    added vrl prices

--- api/service/agents.service.js
+++ api/service/agents.service.js
@@ -214,18 +214,10 @@ export const getAllPendingAgents = async () => {
 export const getAllBookings = async () => {
   try {
     const bookings = await BusBooking.find({ bookingStatus: "paid" }).sort({ createdAt: -1 });
-    const modifiedBookings = bookings.map(booking => {
-      if (booking.isVrl) {
-        booking.busOperator = "VRL travels";
-      }
-      booking.doj = new Date(booking.doj).toISOString().split('T')[0];
-      booking.customerName = booking.customerName + " " + (booking.customerLastName || "");
-      return booking;
-    });
     return {
       status: 200,
       message: "Bookings details retrived",
-      data: modifiedBookings
+      data: bookings
     }
   } catch (error) {
     console.log(error);
@@ -240,18 +232,11 @@ export const getAllBookingRefund = async () => {
   try {
     const bookings = await BusBooking.find({ bookingStatus: "cancelled" })
       .sort({ createdAt: -1 });
-    const modifiedBookings = bookings.map(booking => {
-      if (booking.isVrl) {
-        booking.busOperator = "VRL travels";
-      }
-      booking.doj = new Date(booking.doj).toISOString().split('T')[0];
-      booking.customerName = booking.customerName + " " + (booking.customerLastName || "");
-      return booking;
-    });
+
     return {
       status: 200,
       message: "Bookings details retrived",
-      data: modifiedBookings
+      data: bookings
     }
   } catch (error) {
     console.log(error);
