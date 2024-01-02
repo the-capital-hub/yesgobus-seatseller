c03ebec90094cf69ad8ce519acccdf15c414618a
srihari-m-s <srihari.magadi@gmail.com>
2024-01-02 14:08:05 +0530

    fix: Add fix for table styles in dashboard page

--- client/src/pages/Admin/AdminDashboard/Components/TrackAgentList/TrackAgentList.jsx
+++ client/src/pages/Admin/AdminDashboard/Components/TrackAgentList/TrackAgentList.jsx
@@ -128,6 +128,7 @@ export default function TrackAgentList() {
             spinning:
               !agentPerformanceReport || !agentPerformanceReport.length === 0,
           }}
+          scroll={{ x: true }}
         />
       </ConfigProvider>
       {/* </Spin> */}
