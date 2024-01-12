2dd5a0856983b06b84a120cab063e2f61a42aa1a
srihari-m-s <srihari.magadi@gmail.com>
2024-01-12 14:26:39 +0530

    feat: Add content for security page

--- client/src/pages/SecuritySafeguards/SecuritySafeguards.jsx
+++ client/src/pages/SecuritySafeguards/SecuritySafeguards.jsx
@@ -1,14 +1,77 @@
+import { Link } from "react-router-dom";
 import { Footer, Navbar } from "../../components";
 import "./SecuritySafeguards.scss";
+import { SecurityContent } from "../../constants/SecuritySafeguards";
+import ScrollToTop from "../../components/ScrollToTop/ScrollToTop";
 
 export default function SecuritySafeguards() {
   return (
-    <div className="security-safeguards-wrapper">
-      {/* Navbar */}
-      <Navbar />
-      <main></main>
-      {/* Footer */}
-      <Footer />
-    </div>
+    <>
+      <ScrollToTop />
+      <div className="security-safeguards-wrapper">
+        {/* Navbar */}
+        <Navbar />
+        <main className="security-container">
+          <h1 className="text-primary text-2xl md:text-3xl ">
+            Security Safeguards
+          </h1>
+          <ul className="security-content list-none ps-0">
+            <li className=" mb-4 px-2">
+              <p className="">
+                At{" "}
+                <Link
+                  to="/"
+                  className="no-underline text-primary font-semibold"
+                >
+                  YesGoBus
+                </Link>
+                , we prioritize the security of your personal information. We
+                have implemented robust measures to ensure the confidentiality,
+                integrity, and availability of your data. Below, we outline the
+                security safeguards in place to protect your information:
+              </p>
+            </li>
+
+            {SecurityContent.map(({ heading, text }, index) => {
+              return (
+                <li className="mb-4 px-2" key={heading}>
+                  <h3 className="mb-2">
+                    {index + 1}. {heading}
+                  </h3>
+                  <p className="">{text}</p>
+                </li>
+              );
+            })}
+
+            {/* Contact */}
+            <li className=" mb-4 px-2">
+              <p className="">
+                At{" "}
+                <Link
+                  to="/"
+                  className="no-underline text-primary font-semibold"
+                >
+                  YesGoBus
+                </Link>
+                , your security is our top priority. We are committed to
+                maintaining a secure and trustworthy environment for your travel
+                bookings. If you have any security-related concerns or
+                questions, please contact us at{" "}
+                <a
+                  href="mailto:support@yesgobus.com"
+                  className="no-underline text-primary"
+                >
+                  support@yesgobus.com
+                </a>
+                . Thank you for choosing us for your travel needs, and rest
+                assured that your data is in safe hands.
+              </p>
+            </li>
+          </ul>
+        </main>
+        {/* Footer */}
+        <Footer />
+      </div>
+    </>
   );
 }
--- client/src/pages/TermsOfService/TermsOfService.jsx
+++ client/src/pages/TermsOfService/TermsOfService.jsx
@@ -2,72 +2,81 @@ import { Link } from "react-router-dom";
 import { Footer, Navbar } from "../../components";
 import { TOSContent } from "../../constants/TermsOfService";
 import "./TermsOfService.scss";
+import ScrollToTop from "../../components/ScrollToTop/ScrollToTop";
 
 export default function TermsOfService() {
   return (
-    <div className="terms-of-service-wrapper">
-      {/* Navbar */}
-      <Navbar />
-      <main className="tos-container">
-        <h1 className="text-primary">Terms of Service</h1>
-        <ul className="tos-content list-none ps-0">
-          <li className=" mb-4 px-2">
-            <p className="">
-              Welcome to{" "}
-              <Link to="/" className="no-underline text-primary font-semibold">
-                YesGoBus
-              </Link>
-              . By using our website or mobile application, you agree to comply
-              with and be bound by the following terms and conditions of use.
-              Please read these terms carefully before accessing or using our
-              services.
-            </p>
-          </li>
-          {TOSContent.map(({ heading, text }, index) => {
-            return (
-              <li className="mb-4 px-2" key={heading}>
-                <h3 className="mb-2">
-                  {index + 1}. {heading}
-                </h3>
-                <p className="">{text}</p>
-              </li>
-            );
-          })}
+    <>
+      <ScrollToTop />
+      <div className="terms-of-service-wrapper">
+        {/* Navbar */}
+        <Navbar />
+        <main className="tos-container">
+          <h1 className="text-primary text-2xl md:text-3xl">
+            Terms of Service
+          </h1>
+          <ul className="tos-content list-none ps-0">
+            <li className=" mb-4 px-2">
+              <p className="">
+                Welcome to{" "}
+                <Link
+                  to="/"
+                  className="no-underline text-primary font-semibold"
+                >
+                  YesGoBus
+                </Link>
+                . By using our website or mobile application, you agree to
+                comply with and be bound by the following terms and conditions
+                of use. Please read these terms carefully before accessing or
+                using our services.
+              </p>
+            </li>
+            {TOSContent.map(({ heading, text }, index) => {
+              return (
+                <li className="mb-4 px-2" key={heading}>
+                  <h3 className="mb-2">
+                    {index + 1}. {heading}
+                  </h3>
+                  <p className="">{text}</p>
+                </li>
+              );
+            })}
 
-          {/* Privacy Policy */}
-          <li className="mb-4 px-2">
-            <h3 className="mb-2">{TOSContent.length + 1}. Privacy Policy</h3>
-            <p className="">
-              Your privacy is important to us. Please review our{" "}
-              <Link to={"/privacy"} className="no-underline text-primary">
-                Privacy Policy
-              </Link>{" "}
-              to understand how we collect, use, and disclose your personal
-              information.
-            </p>
-          </li>
+            {/* Privacy Policy */}
+            <li className="mb-4 px-2">
+              <h3 className="mb-2">{TOSContent.length + 1}. Privacy Policy</h3>
+              <p className="">
+                Your privacy is important to us. Please review our{" "}
+                <Link to={"/privacy"} className="no-underline text-primary">
+                  Privacy Policy
+                </Link>{" "}
+                to understand how we collect, use, and disclose your personal
+                information.
+              </p>
+            </li>
 
-          {/* Contact information */}
-          <li className="mb-4 px-2">
-            <h3 className="mb-2">
-              {TOSContent.length + 2}. Contact Information
-            </h3>
-            <p className="">
-              If you have any questions about these terms of service, please
-              contact us at{" "}
-              <a
-                href="mailto:support@yesgobus.com"
-                className="no-underline text-primary"
-              >
-                support@yesgobus.com
-              </a>
-              .
-            </p>
-          </li>
-        </ul>
-      </main>
-      {/* Footer */}
-      <Footer />
-    </div>
+            {/* Contact information */}
+            <li className="mb-4 px-2">
+              <h3 className="mb-2">
+                {TOSContent.length + 2}. Contact Information
+              </h3>
+              <p className="">
+                If you have any questions about these terms of service, please
+                contact us at{" "}
+                <a
+                  href="mailto:support@yesgobus.com"
+                  className="no-underline text-primary"
+                >
+                  support@yesgobus.com
+                </a>
+                .
+              </p>
+            </li>
+          </ul>
+        </main>
+        {/* Footer */}
+        <Footer />
+      </div>
+    </>
   );
 }
