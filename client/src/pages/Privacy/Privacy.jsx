b7b9b464096719940be764347a68b9cfe6038fcf
srihari-m-s <srihari.magadi@gmail.com>
2024-01-12 13:59:40 +0530

    feat: Add content for Privacy Policy

--- client/src/pages/Privacy/Privacy.jsx
+++ client/src/pages/Privacy/Privacy.jsx
@@ -1,12 +1,76 @@
+import { Link } from "react-router-dom";
 import { Footer, Navbar } from "../../components";
 import "./Privacy.scss";
+import { PrivacyContent } from "../../constants/Privacy";
 
 export default function Privacy() {
   return (
     <div className="privacy-wrapper">
       {/* Navbar */}
       <Navbar />
-      <p>Privacy</p>
+      <main className="privacy-container">
+        <h1 className="text-primary">Privacy Policy</h1>
+        <ul className="privacy-content list-none ps-0">
+          <li className=" mb-4 px-2">
+            <p className="">
+              Welcome to{" "}
+              <Link to="/" className="no-underline text-primary font-semibold">
+                YesGoBus
+              </Link>
+              . This Privacy Policy explains how we collect, use, and protect
+              the personal information you provide while using our website or
+              mobile application.
+            </p>
+          </li>
+
+          {PrivacyContent.map(({ heading, text, list }, index) => {
+            return (
+              <li className="mb-4 px-2" key={heading}>
+                <h3 className="mb-2">
+                  {index + 1}. {heading}
+                </h3>
+                <p className="">{text}</p>
+                {list && list.length ? (
+                  <ul className="mt-2">
+                    {list.map((text) => {
+                      return (
+                        <li className="mb-4" key={text}>
+                          {text}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                ) : (
+                  ""
+                )}
+              </li>
+            );
+          })}
+
+          {/* Contact information */}
+          <li className="mb-4 px-2">
+            <h3 className="mb-2">
+              {PrivacyContent.length + 1}. Contact Information
+            </h3>
+            <p className="">
+              If you have any questions about this Privacy Policy, please
+              contact us at{" "}
+              <a
+                href="mailto:support@yesgobus.com"
+                className="no-underline text-primary"
+              >
+                support@yesgobus.com
+              </a>
+              . Thank you for trusting{" "}
+              <Link to="/" className="no-underline text-primary font-semibold">
+                YesGoBus
+              </Link>
+              . We are committed to safeguarding your privacy and providing you
+              with a secure and enjoyable booking experience.
+            </p>
+          </li>
+        </ul>
+      </main>
       {/* Footer */}
       <Footer />
     </div>
--- client/src/pages/TermsOfService/TermsOfService.jsx
+++ client/src/pages/TermsOfService/TermsOfService.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Footer, Navbar } from "../../components";
 import { TOSContent } from "../../constants/TermsOfService";
 import "./TermsOfService.scss";
@@ -9,10 +10,22 @@ export default function TermsOfService() {
       <Navbar />
       <main className="tos-container">
         <h1 className="text-primary">Terms of Service</h1>
-        <ul className="md:max-w-[80dvw] ps-0">
+        <ul className="tos-content list-none ps-0">
+          <li className=" mb-4 px-2">
+            <p className="">
+              Welcome to{" "}
+              <Link to="/" className="no-underline text-primary font-semibold">
+                YesGoBus
+              </Link>
+              . By using our website or mobile application, you agree to comply
+              with and be bound by the following terms and conditions of use.
+              Please read these terms carefully before accessing or using our
+              services.
+            </p>
+          </li>
           {TOSContent.map(({ heading, text }, index) => {
             return (
-              <li className="list-none mb-4 px-2" key={heading}>
+              <li className="mb-4 px-2" key={heading}>
                 <h3 className="mb-2">
                   {index + 1}. {heading}
                 </h3>
@@ -20,9 +33,24 @@ export default function TermsOfService() {
               </li>
             );
           })}
-          <li className="list-none mb-4 px-2">
+
+          {/* Privacy Policy */}
+          <li className="mb-4 px-2">
+            <h3 className="mb-2">{TOSContent.length + 1}. Privacy Policy</h3>
+            <p className="">
+              Your privacy is important to us. Please review our{" "}
+              <Link to={"/privacy"} className="no-underline text-primary">
+                Privacy Policy
+              </Link>{" "}
+              to understand how we collect, use, and disclose your personal
+              information.
+            </p>
+          </li>
+
+          {/* Contact information */}
+          <li className="mb-4 px-2">
             <h3 className="mb-2">
-              {TOSContent.length + 1}. {"Contact Information"}
+              {TOSContent.length + 2}. Contact Information
             </h3>
             <p className="">
               If you have any questions about these terms of service, please
