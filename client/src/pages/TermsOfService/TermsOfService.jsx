7c5270aab6fc816fa3828ee43c2abfc94c3e7e63
srihari-m-s <srihari.magadi@gmail.com>
2024-01-12 13:11:07 +0530

    feat: Add content for TOS

--- client/src/pages/TermsOfService/TermsOfService.jsx
+++ client/src/pages/TermsOfService/TermsOfService.jsx
@@ -1,4 +1,5 @@
 import { Footer, Navbar } from "../../components";
+import { TOSContent } from "../../constants/TermsOfService";
 import "./TermsOfService.scss";
 
 export default function TermsOfService() {
@@ -7,7 +8,35 @@ export default function TermsOfService() {
       {/* Navbar */}
       <Navbar />
       <main className="tos-container">
-        <h1>Terms of Service</h1>
+        <h1 className="text-primary">Terms of Service</h1>
+        <ul className="xl:max-w-[80dvw] ps-0">
+          {TOSContent.map(({ heading, text }, index) => {
+            return (
+              <li className="list-none mb-4 px-2" key={heading}>
+                <h3 className="mb-2">
+                  {index + 1}. {heading}
+                </h3>
+                <p className="">{text}</p>
+              </li>
+            );
+          })}
+          <li className="list-none mb-4 px-2">
+            <h3 className="mb-2">
+              {TOSContent.length + 1}. {"Contact Information"}
+            </h3>
+            <p className="">
+              If you have any questions about these terms of service, please
+              contact us at{" "}
+              <a
+                href="mailto:support@yesgobus.com"
+                className="no-underline text-primary"
+              >
+                support@yesgobus.com
+              </a>
+              .
+            </p>
+          </li>
+        </ul>
       </main>
       {/* Footer */}
       <Footer />
