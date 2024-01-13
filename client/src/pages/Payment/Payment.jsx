b75474e29d8f8e6aff9b13ae276f1782b3db022a
Adithya <adithyahebbar32@gmail.com>
2024-01-14 01:38:28 +0530

    fixes

--- client/src/pages/Payment/Payment.jsx
+++ client/src/pages/Payment/Payment.jsx
@@ -817,8 +817,8 @@ const Payment = () => {
           const response = await axiosInstance.post(
             `${import.meta.env.VITE_BASE_URL}/api/payment/initiatePayment`,
             {
-              // amount: bookingDetails?.totalFare,
-              amount: 1,
+              amount: bookingDetails?.totalFare,
+              // amount: 1,
               redirectUrl: `https://yesgobus.com/busbooking/payment?blockTicketId=${srsResponse.pnr_number}&bookingId=${bookResponse.data._id}&paymentVerify=1`,
             }
           );
