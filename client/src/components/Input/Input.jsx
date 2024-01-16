c334015b02a98f1abf1eb8af24e2555561c0c907
MuhamedShahabasC <muhamedshahabasc@gmail.com>
2024-01-16 14:43:42 +0530

    * added to required fields and updated customer care number

--- client/src/components/Input/Input.jsx
+++ client/src/components/Input/Input.jsx
@@ -1,22 +1,32 @@
 import "./Input.scss";
 
-const Input = ({ title, type, placeholder, onChanged, givenName, isKyc, value }) => {
+const Input = ({
+  title,
+  type,
+  placeholder,
+  onChanged,
+  givenName,
+  isKyc,
+  value,
+  required,
+}) => {
   const handleChange = (e) => {
     e.preventDefault();
     onChanged((prev) => {
       return { ...prev, [givenName]: e.target.value };
     });
   };
-  const handleOnChange = isKyc ? handleChange : onChanged;  
+  const handleOnChange = isKyc ? handleChange : onChanged;
   return (
     <div className="Input">
-      <span >{title}</span>
+      <span className="title">{title}</span>
       <input
         name={givenName}
         onChange={handleOnChange}
         type={type}
         placeholder={placeholder}
         value={value}
+        required={required || false}
       />
     </div>
   );
