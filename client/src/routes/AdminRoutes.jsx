0e92bfa295664d11d99f16516d559b495e09499d
srihari-m-s <srihari.magadi@gmail.com>
2024-01-20 16:33:16 +0530

    fix: Add redirection to 404 from adminRoutes

--- client/src/routes/AdminRoutes.jsx
+++ client/src/routes/AdminRoutes.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 import AdminTrackAgent from "../pages/Admin/AdminTrackAgent/AdminTrackAgent";
 // import AdminTrackBuses from "../pages/Admin/AdminTrackBuses/AdminTrackBuses";
@@ -17,6 +17,7 @@ export default function AdminRoutes() {
         <Route path="wallet" element={<AdminWallet />} />
         <Route path="records" element={<AdminRecords />} />
         <Route path="track-bdas" element={<AdminTrackAgent />} />
+        <Route path="*" element={<Navigate to={"/not-found"} replace />} />
         {/* <Route path="track-buses" element={<AdminTrackBuses />} /> */}
       </Route>
     </Routes>
